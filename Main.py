540b1f6cce7d91081a7d6fe38bdee7f624a44bd9
José Victor B D B R <89922069+josevictorbdbr@users.noreply.github.com>
2025-08-19 11:10:07 -0300

    Add Ficha para visualização / Add alocação da escolha do usuário

--- Main.py
+++ Main.py
@@ -1,3 +1,13 @@
+class Personagem:
+    def __init__(self, nome, estiloDist, estiloRaca):
+        self.nome = nome
+        self.estiloDist = estiloDist
+        self.estiloRaca = estiloRaca
+
+    def exibirFicha(self):
+        print(f"\n == Personagem: ({self.nome} / ({self.estiloRaca})) == ")
+        print(f" == Modo: ({self.estiloDist}) == ")
+
 def main():
     nome = input("Digite o nome do personagem: ")
 
@@ -5,11 +15,15 @@ def main():
     print("Escolha uma raça para esse personagem: ")
     print("(1) Humano / (2) Elfo / (3) Halfling / (4) Anão")
     escolhaRaca = input("Opção: ")
-    estilo = { "1": "Humano", "2": "Elfo", "3": "Halfling", "4": "Anão" }.get(escolhaRaca, "Humano")
+    estiloRaca = { "1": "Humano", "2": "Elfo", "3": "Halfling", "4": "Anão" }.get(escolhaRaca, "Humano")
 
     print("Escolha o estilo de distribuição: (1) Clássico | (2) Aventureiro | (3) Heroico")
     escolhaDis = input("Opção: ")
-    estilo = { "1": "classico", "2": "aventureiro", "3": "heroico" }.get(escolhaDis, "classico")
+    estiloDist = { "1": "classico", "2": "aventureiro", "3": "heroico" }.get(escolhaDis, "classico")
+
+    personagem = Personagem(nome, estiloDist, estiloRaca)
+    personagem.exibirFicha()
+
 
 if __name__ == "__main__":
     main()
