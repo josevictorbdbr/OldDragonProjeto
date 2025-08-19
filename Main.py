7bd970ca706c0465f98bb3a3a8b3c8b1739069be
José Victor B D B R <89922069+josevictorbdbr@users.noreply.github.com>
2025-08-19 10:25:49 -0300

    Add escolha do usuário

--- Main.py
+++ Main.py
@@ -1,2 +1,15 @@
-print("Teste para GitHub")
-printf("Teste 2")
+def main():
+    nome = input("Digite o nome do personagem: ")
+
+    #Pergunta ao usuario e transforma a escolha que foi usado numeros para uma string
+    print("Escolha uma raça para esse personagem: ")
+    print("(1) Humano / (2) Elfo / (3) Halfling / (4) Anão")
+    escolhaRaca = input("Opção: ")
+    estilo = { "1": "Humano", "2": "Elfo", "3": "Halfling", "4": "Anão" }.get(escolhaRaca, "Humano")
+
+    print("Escolha o estilo de distribuição: (1) Clássico | (2) Aventureiro | (3) Heroico")
+    escolhaDis = input("Opção: ")
+    estilo = { "1": "classico", "2": "aventureiro", "3": "heroico" }.get(escolhaDis, "classico")
+
+if __name__ == "__main__":
+    main()
