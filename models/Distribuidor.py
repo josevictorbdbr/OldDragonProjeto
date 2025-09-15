b037b93edf399a01b0f29e718e8a011982b5a369
josevictorbdbr <josevictorbdbr@gmail.com>
2025-09-15 10:19:16 -0300

    Repetição de codigo no Distribuidor concertada

--- models/Distribuidor.py
+++ models/Distribuidor.py
@@ -4,33 +4,32 @@ class DistribuidorAtributos:
 
     #Dados d6
     def rolar_3d6(self):
-        return sum(random.randint(1, 6) for _ in range(3)) 
+        return sum(random.randint(1, 6) for _ in range(3))
     def rolar_4d6_descartar_menor(self):
         dados = [random.randint(1, 6) for _ in range(4)]
         dados.remove(min(dados))
         return sum(dados)
-    
-    #Rolar dado no estilo escolhido, aventureiro e heroico
+
     def dado_classico(self):
         return [self.rolar_3d6() for _ in range(6)]
-    
     def dado_heroico(self):
         return [self.rolar_4d6_descartar_menor() for _ in range(6)]
     
+    #Pega a escolha de estilo, roda os dados e manda para escolha personalizada
     def escolha_aventureiro(self):
         dados = self.dado_classico()
-        escolha_atributos = input(
-            f"\nVocê tirou os dados: {dados}\n"
-            f"Ordem: [FORÇA, DESTREZA, CONSTITUIÇÃO, INTELIGENCIA, SABEDORIA, CARISMA]\n"
-            f"Digite um dos numeros tirados para cada atributo, separados por virgula e baseando-se na ordem acima: ")
-        valores = [int(x.strip()) for x in escolha_atributos.split(",")]
-        return valores
-    
+        return self.escolha_personalizada(dados)
     def escolha_heroico(self):
         dados = self.dado_heroico()
-        escolha_atributos = input(
+        return self.escolha_personalizada(dados)
+    
+    #Usuario faz a escolha de onde colocar atributos caso seja estilo heroico ou aventureiro
+    def escolha_personalizada(self, dados):
+        escolha = input(
             f"\nVocê tirou os dados: {dados}\n"
-            f"Ordem: [FORÇA, DESTREZA, CONSTITUIÇÃO, INTELIGENCIA, SABEDORIA, CARISMA]\n"
-            f"Digite um dos numeros tirados para cada atributo, separados por virgula e baseando-se na ordem acima: ")
-        valores = [int(x.strip()) for x in escolha_atributos.split(",")]
+            "Ordem: [FORÇA, DESTREZA, CONSTITUIÇÃO, INTELIGENCIA, SABEDORIA, CARISMA]\n"
+            "Digite um dos numeros tirados para cada atributo, separados por vírgula: "
+        )
+        valores = [int(x.strip()) for x in escolha.split(",")]
         return valores
+    
